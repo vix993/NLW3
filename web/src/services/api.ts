fbea6b6be379e1ade3f8935e82401ee8bf33adc4
Victor Nascimento <52217652+vix993@users.noreply.github.com>
2020-10-16 13:13:51 -0300

    Update api.ts

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://api-h-appy.herokuapp.com/',
+    baseURL: 'https://api-h-appy.herokuapp.com/',
 })
 
 export default api;
